5f9c9d771af4d57bce830e5729150b7f274d0cc4
David Megginson <david.megginson@megginson.com>
2015-07-27 09:57:10 +0000

    Working on CRS example.

--- Templates/Examples/make-crs.sh
+++ Templates/Examples/make-crs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TEMPLATE="../PCI modele 2015-07-27.xlsx"
-SOURCE="../../Tagged/Jhpiego IPC uploads/Activites des partenaires par prefectures et sous  prefectures _ 220615_Jhpiego.xlsx"
+SOURCE="../../Tagged/CRS IPC uploads/Activites des partenaires par prefectures et sous prefectures _ CRS.xlsx"
 PREFECTURES=$HOME/gin-master-facilities/prefectures.csv
 CORRECTIONS=../../Inputs/corrections-table.csv
 REPLACEMENTS=../../Inputs/replacements.csv
@@ -10,23 +10,20 @@ echo Processing public facilities ...
 
 hxlclean --sheet 1 "$SOURCE" \
     | hxlselect -q 'org!~^$' \
-    | hxladd -b -s '#org+code=Jhpiego' \
+    | hxladd -b -s 'org+code=CRS' \
     | hxlreplace -m $CORRECTIONS \
     | hxlreplace -m $REPLACEMENTS \
-    | hxlrename -r org:org+name \
     | hxlrename -r adm2:adm2+name \
     | hxlrename -r adm3:adm3+name \
     | hxlrename -r loc:loc+name \
                 > Working/temp1.csv
 
 echo Processing private facilities ...
-
 hxlclean --sheet 2 "$SOURCE" \
     | hxlselect -q 'org!~^$' \
-    | hxladd -b -s '#org+code=Jhpiego' \
+    | hxladd -b -s '#org+code=CRS' \
     | hxlreplace --sheet 2 -m $CORRECTIONS \
     | hxlreplace -m $REPLACEMENTS \
-    | hxlrename -r org:org+name \
     | hxlrename -r adm2:adm2+name \
     | hxlrename -r adm3:adm3+name \
     | hxlrename -r loc:loc+name \
@@ -40,7 +37,7 @@ cat "$TEMPLATE" \
     | hxlappend -x -a Working/temp2.csv \
     | hxlmerge -r -k adm2 -t adm1+name -m $PREFECTURES \
     | hxlsort \
-                > jhpiego-example.csv
+                > crs-example.csv
 
 
 rm -rf Working/*
