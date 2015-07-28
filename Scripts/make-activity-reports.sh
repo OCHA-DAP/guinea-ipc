7b91d8a586c05a043db7fa2d0955aa874a7ce97c
David Megginson <david.megginson@megginson.com>
2015-07-28 16:57:28 +0000

    Added masterdata for prefectures and regions.
    Activity report script also generates empty admin units (no activity)
    to help with mapping.

--- Scripts/make-activity-reports.sh
+++ Scripts/make-activity-reports.sh
@@ -15,21 +15,33 @@ function do_reports {
              | hxlselect -q 'adm3+name!~^$' \
              | hxlselect -q 'date!~^$' \
              | hxlcount -t adm1+name,adm1+code,adm2+name,adm2+code,adm3+name,adm3+code \
-                      > "Reports/$2-adm3-all.csv"
+                        > "Reports/$2-adm3-reported.csv"
+
+         hxlmerge -m "Reports/$2-adm3-reported.csv" -k adm1+code,adm2+code,adm3+code -t meta+count Inputs/subprefectures.csv \
+                  | hxlreplace -t meta+count -r -p '^$' -s '0' \
+                  > "Reports/$2-adm3-all.csv"
 
          cat "$1" \
              | sh Scripts/add-pcodes.sh \
              | hxlselect -q 'adm2+name!~^$' \
              | hxlselect -q 'date!~^$' \
              | hxlcount -t adm1+name,adm1+code,adm2+name,adm2+code \
-                      > "Reports/$2-adm2-all.csv"
+                      > "Reports/$2-adm2-reported.csv"
+
+         hxlmerge -m "Reports/$2-adm2-reported.csv" -k adm1+code,adm2+code -t meta+count Inputs/prefectures.csv \
+                  | hxlreplace -t meta+count -r -p '^$' -s '0' \
+                  > "Reports/$2-adm2-all.csv"
 
          cat "$1" \
              | sh Scripts/add-pcodes.sh \
              | hxlselect -q 'adm1+name!~^$' \
              | hxlselect -q 'date!~^$' \
              | hxlcount -t adm1+name,adm1+code \
-                      > "Reports/$2-adm1-all.csv"
+                      > "Reports/$2-adm1-reported.csv"
+
+         hxlmerge -m "Reports/$2-adm1-reported.csv" -k adm1+code -t meta+count Inputs/regions.csv \
+                  | hxlreplace -t meta+count -r -p '^$' -s '0' \
+                  > "Reports/$2-adm1-all.csv"
 
 }
 
